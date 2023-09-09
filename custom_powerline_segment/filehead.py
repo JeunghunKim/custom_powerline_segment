348220b4eff747d9571d3d67f4668d8ca0f1c08d
Jeunghun Kim <nanan100410@gmail.com>
2023-09-10 08:58:04 +0900

    Add filehead segment

---
+++ custom_powerline_segment/filehead.py
@@ -0,0 +1,11 @@
+from powerline.theme import requires_segment_info
+
+
+@requires_segment_info
+def environment(pl, segment_info, filepath=None):
+    try:
+        with open(filepath, 'r') as f:
+            file = f.read().splitlines()
+    except Exception as e:
+        return str(filepath)
+    return file[0]
---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+	name="custom_powerline_segment",
+	version="0.0.1",
+	author="Jeunghun Kim",
+	author_email="nanan100410@gmail.com",
+	description="Custom powerline segments",
+	url="https://github.com/JeunghunKim/custom_powerline_segment",
+	license="MIT",
+	packages=setuptools.find_packages(),
+)
+
