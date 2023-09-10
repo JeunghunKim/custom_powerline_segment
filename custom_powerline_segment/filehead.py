17434498731a4877a5aef2437c3277ed79ae7f78
Jeunghun Kim <nanan100410@gmail.com>
2023-09-10 09:17:02 +0900

    Add username to filepath

--- custom_powerline_segment/filehead.py
+++ custom_powerline_segment/filehead.py
@@ -3,8 +3,9 @@ from powerline.theme import requires_segment_info
 
 @requires_segment_info
 def environment(pl, segment_info, filepath=None):
+    user_name = segment_info['environ'].get('USER', None)
     try:
-        with open(filepath, 'r') as f:
+        with open('/tmp/{}_{}'.(user_name, filepath), 'r') as f:
             file = f.read().splitlines()
     except Exception as e:
         return None
